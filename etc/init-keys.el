7d8e12e1117337f7d3bc4c97105633c1af43c4e8
David Rousselie <david.rousselie@happycoders.org>
2012-12-29 17:51:45 +0100

    Add convenient rename file function

--- etc/init-keys.el
+++ etc/init-keys.el
@@ -154,4 +154,23 @@ the character typed."
 	(t                    (self-insert-command (or arg 1))) ))
 (global-set-key "%" 'goto-match-paren)
 
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+
 (provide 'init-keys)
