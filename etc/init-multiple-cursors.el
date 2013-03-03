adbf8fa85b5965f1f460147229197f5fea185825
David Rousselie <david.rousselie@happycoders.org>
2013-03-03 16:38:36 +0100

    Fix multiple cursor configuration

--- etc/init-multiple-cursors.el
+++ etc/init-multiple-cursors.el
@@ -4,10 +4,4 @@
 (global-set-key (kbd "C-*") 'mc/mark-all-like-this)
 (global-set-key (kbd "M-SPC") 'set-rectangular-region-anchor)
 
-(eval-after-load "sgml-mode"
-  '(progn
-     (require 'rename-sgml-tag)
-     (define-key sgml-mode-map (kbd "C-c C-r") 'rename-sgml-tag)
-     ))
-
 (provide 'init-multiple-cursors)
